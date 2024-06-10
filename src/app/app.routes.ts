ae89ad037ee7df739964f0f19c4a631e216f2707
seduocuc2024 <se.penap@duocuc.cl>
2024-06-10 16:54:18 -0400

    mi primer commit del proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AventuraComponent } from './aventura/aventura.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { DosjugadoresComponent } from './dosjugadores/dosjugadores.component';
+import { FamiliarComponent } from './familiar/familiar.component';
+import { LoginComponent } from './login/login.component';
+import { NinosComponent } from './ninos/ninos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'aventura', component: AventuraComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'dosjugadores', component: DosjugadoresComponent },
+  { path: 'familiar', component: FamiliarComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'ninos', component: NinosComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
